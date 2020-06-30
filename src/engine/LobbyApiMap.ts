98753c0d7aa3e4b0f795320bd14a13282d813487
Ilia Feldgun <iliafeldgun@outlook.com>
2020-06-30 17:11:00 +0300

    API access is now to a different server

--- src/engine/LobbyApiMap.ts
+++ src/engine/LobbyApiMap.ts
@@ -1,12 +1,15 @@
 import { AxiosRequestConfig } from 'axios'
 
+const API_ENPOINT = process.env.REACT_APP_CLUB_API_SERVER
+
 function GET_CONFIG(): AxiosRequestConfig {
     return {
         method: "GET",
         headers: {
             'Content-Type': 'application/json',
             'Cache-Control': 'no-cache'
-        }
+        },
+        withCredentials: true
     }
 }
 function POST_CONFIG(): AxiosRequestConfig {
@@ -15,7 +18,8 @@ function POST_CONFIG(): AxiosRequestConfig {
         headers: {
             'Content-Type': 'application/json',
             'Cache-Control': 'no-cache'
-        }
+        },
+        withCredentials: true
     }
 }
 
@@ -24,45 +28,50 @@ const LOBBY_API_MAP = {
         GET_ROOM: {
             config: GET_CONFIG,
             url: (roomId: string) => {
-                return `/api/room/${roomId}`
+                return `${API_ENPOINT}/api/room/${roomId}`
             }
         },
         CREATE_ROOM: {
             config: POST_CONFIG,
             url: () => {
-                return `/api/room`
+                return `${API_ENPOINT}/api/room`
             }
         },
         JOIN_ROOM: {
             config: POST_CONFIG,
             url: (roomId: string) => {
-                return `/api/room/${roomId}/join`
+                return `${API_ENPOINT}/api/room/${roomId}/join`
             }
         },
         GET_LEADER: {
             config: GET_CONFIG,
             url: (roomId: string) => {
-                return `/api/room/${roomId}/leader`
+                return `${API_ENPOINT}/api/room/${roomId}/leader`
             }
         },
         GET_PLAYER_NAMES: {
             config: GET_CONFIG,
             url: (roomId: string) => {
-                return `/api/room/${roomId}/playernames`
+                return `${API_ENPOINT}/api/room/${roomId}/playernames`
             }
         },
         GET_GAME: {
             config: GET_CONFIG,
             url: (roomId: string) => {
-                return `/api/room/${roomId}/game`
+                return `${API_ENPOINT}/api/room/${roomId}/game`
             }
         },
+        UPDATES: {
+            url: () => {
+                return `${API_ENPOINT}/api/room/updates`
+            }
+        }
     },
     PLAYER: {
         GET_ROOMS: {
             config: GET_CONFIG,
             url: () => {
-                return `/api/player/rooms`
+                return `${API_ENPOINT}/api/player/rooms`
             }
         },
         CREATE_PLAYER: {
@@ -70,7 +79,7 @@ const LOBBY_API_MAP = {
                 return {...POST_CONFIG(), data: {playerName}}
             },
             url: () => {
-                return `/api/player`
+                return `${API_ENPOINT}/api/player`
             },
         }
     }

b78755d7ad8f0e21df0157694930bbb4253f359d
Ilia Feldgun <iliafeldgun@outlook.com>
2020-07-03 17:20:09 +0300

    Changed describe message

--- src/test/components/Login/LogoutFrom.test.tsx
+++ src/test/components/Login/LogoutFrom.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import LogoutForm from '../../../components/Login/LogoutForm'
 
-describe('Testing LoginForm', () => {
+describe('Testing LogoutForm', () => {
     test('component renders', () => {
         const div = document.createElement('div')
         ReactDOM.render(<LogoutForm />, div)

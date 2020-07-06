50f529fded8d0ce6aacbf3395af4c767cc576226
Ilia Feldgun <iliafeldgun@outlook.com>
2020-07-06 18:39:08 +0300

    Moved header and footer into app

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,40 @@ import Room from "./routes/Room"
 import Lobby from "./routes/Lobby"
 import Rooms from "./routes/RoomCreate"
 import Profile from "./routes/Profile"
+import Footer from './components/Main/Footer'
+import Header from './components/Main/Header'
+import UserName from './components/Main/UserName'
 
 export default class App extends React.PureComponent {
     render() {
         return (
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/">
-                        <Home/>
-                    </Route>
-                    <Route exact path="/wizard/:id" component={Wiz}>
-                    </Route>
-                    <Route exact path="/login">
-                        <Login/>
-                    </Route>
-                    <Route exact path="/lobby">
-                        <Lobby/>
-                    </Route>
-                    <Route exact path="/room/:id" component={Room}>
-                    </Route>
-                    <Route exact path="/room">
-                        <Rooms/>
-                    </Route>
-                    <Route exact path="/profile" component={Profile} />
-                </Switch>
-            </BrowserRouter>
+            <React.Fragment>
+                <Header>
+                <UserName />
+                </Header>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/">
+                            <Home/>
+                        </Route>
+                        <Route exact path="/wizard/:id" component={Wiz}>
+                        </Route>
+                        <Route exact path="/login">
+                            <Login/>
+                        </Route>
+                        <Route exact path="/lobby">
+                            <Lobby/>
+                        </Route>
+                        <Route exact path="/room/:id" component={Room}>
+                        </Route>
+                        <Route exact path="/room">
+                            <Rooms/>
+                        </Route>
+                        <Route exact path="/profile" component={Profile} />
+                    </Switch>
+                </BrowserRouter>
+                <Footer />
+            </React.Fragment>
         )
     }
 }

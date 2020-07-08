93c2928992b2b9d1d25e113bce2275341f43a3f4
Ilia Feldgun <iliafeldgun@outlook.com>
2020-07-08 20:20:33 +0300

    Error boundary added

--- src/App.tsx
+++ src/App.tsx
@@ -10,36 +10,39 @@ import Profile from "./routes/Profile"
 import Footer from './components/Main/Footer'
 import Header from './components/Main/Header'
 import UserName from './components/Main/UserName'
+import ErrorBoundary from "./components/ErrorBoundary"
 
 export default class App extends React.PureComponent {
     render() {
         return (
             <React.Fragment>
-                <Header>
-                <UserName />
-                </Header>
-                <BrowserRouter>
-                    <Switch>
-                        <Route exact path="/">
-                            <Home/>
-                        </Route>
-                        <Route exact path="/wizard/:id" component={Wiz}>
-                        </Route>
-                        <Route exact path="/login">
-                            <Login/>
-                        </Route>
-                        <Route exact path="/lobby">
-                            <Lobby/>
-                        </Route>
-                        <Route exact path="/room/:id" component={Room}>
-                        </Route>
-                        <Route exact path="/room">
-                            <Rooms/>
-                        </Route>
-                        <Route exact path="/profile" component={Profile} />
-                    </Switch>
-                </BrowserRouter>
-                <Footer />
+                <ErrorBoundary>
+                    <Header>
+                    <UserName />
+                    </Header>
+                    <BrowserRouter>
+                        <Switch>
+                            <Route exact path="/">
+                                <Home/>
+                            </Route>
+                            <Route exact path="/wizard/:id" component={Wiz}>
+                            </Route>
+                            <Route exact path="/login">
+                                <Login/>
+                            </Route>
+                            <Route exact path="/lobby">
+                                <Lobby/>
+                            </Route>
+                            <Route exact path="/room/:id" component={Room}>
+                            </Route>
+                            <Route exact path="/room">
+                                <Rooms/>
+                            </Route>
+                            <Route exact path="/profile" component={Profile} />
+                        </Switch>
+                    </BrowserRouter>
+                    <Footer />
+                </ErrorBoundary>
             </React.Fragment>
         )
     }

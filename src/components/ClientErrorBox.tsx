70c1613282fd26775c8861799251a5606a0f569a
Ilia Feldgun <iliafeldgun@outlook.com>
2020-08-04 19:19:11 +0300

    Converted to FunctionComponent

--- src/components/ClientErrorBox.tsx
+++ src/components/ClientErrorBox.tsx
@@ -10,43 +10,36 @@ interface IClientErrorBoxProps {
 interface IClientErrorBoxState {
     displayError: boolean
 }
-export default class ClientErrorBox extends React.PureComponent<IClientErrorBoxProps, IClientErrorBoxState>{
-    constructor(props: IClientErrorBoxProps) {
-        super(props)
-        this.state = {
-            displayError: true
-        }
-    }
-    handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        this.handleOpenModal()
-    }
-    handleOpenModal = () => {
-        this.setState({ displayError: true })
+const ClientErrorBox: React.FC<IClientErrorBoxProps> = (props) => {
+    const [displayError, setDisplayError] = React.useState(true)
+    
+    const handleOpenModal = () => {
+        setDisplayError(true)
     }
     
-    handleCloseModal = () => {
-        this.setState({ displayError: false })
-        if (this.props.onModalClose) {
-            this.props.onModalClose()
+    const handleCloseModal = () => {
+        setDisplayError(false)
+        if (props.onModalClose) {
+            props.onModalClose()
         }
     }
-    render() {
-        return (
-            <React.Fragment>
-                <ReactModal 
-                    className="box-modal error-modal"
-                    shouldCloseOnEsc={true}
-                    shouldCloseOnOverlayClick={true}
-                    isOpen={this.state.displayError}
-                    onRequestClose={this.handleCloseModal}
-                >
-                    <h1>{this.props.error.httpStatusCode}</h1>
-                    <h3>{this.props.error.message}</h3>
-                    <button className="form-button close-button" onClick={this.handleCloseModal}>
-                        Close
-                    </button>
-                </ReactModal>
-            </React.Fragment>
-        )
-    }
+    return (
+        <React.Fragment>
+            <ReactModal 
+                className="box-modal error-modal"
+                shouldCloseOnEsc={true}
+                shouldCloseOnOverlayClick={true}
+                isOpen={displayError}
+                onRequestClose={handleCloseModal}
+            >
+                <h1>{props.error.httpStatusCode}</h1>
+                <h3>{props.error.message}</h3>
+                <button className="form-button close-button" onClick={handleCloseModal}>
+                    Close
+                </button>
+            </ReactModal>
+        </React.Fragment>
+    )
 }
+
+export default ClientErrorBox

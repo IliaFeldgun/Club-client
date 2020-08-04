5d258a9b1ade8f11571485f98071792d1dac99d6
Ilia Feldgun <iliafeldgun@outlook.com>
2020-08-04 15:29:35 +0300

    Converted to FunctionComponent

--- src/wiz/components/PlaceholderCard.tsx
+++ src/wiz/components/PlaceholderCard.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 
-export default class PlaceholderCard extends React.PureComponent {
-    render() {
-        const classes = `player-card placeholder-card`
-        return (<p className={classes}/>)
-    }
-
+const PlaceholderCard: React.FC = () => {
+    const classes = `player-card placeholder-card`
+    return (<p className={classes}/>)
 }

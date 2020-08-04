a2537b799090e73684d18f11c7922319c771b752
Ilia Feldgun <iliafeldgun@outlook.com>
2020-08-04 19:47:33 +0300

    Converted to FunctionComponent

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -6,44 +6,31 @@ import ClubSession from '../utils/ClubSession'
 import { Redirect } from "react-router-dom"
 import ClientError from '../engine/api/ClientError'
 
-interface IProfileProps {
-
-}
-interface IProfileState {
-    rooms: string[]
-}
-export default class Profile extends React.PureComponent<IProfileProps,IProfileState> {
-    constructor(props: IProfileProps) {
-        super(props)
-
-        this.state = {
-            rooms: []
-        }
-    }
-
-    componentDidMount() {
+const Profile: React.FC = () => {
+    const [rooms, setRooms] = React.useState<string[]>([])
+    React.useEffect(() => {
         LobbyApi.getPlayerRooms().then(
-            (rooms) => this.setState(() => ({rooms}))
+            (playerRooms) => setRooms(() => (playerRooms))
         ).catch((error: ClientError) => {
             // TODO: Handle better
             console.error(`${error.httpStatusCode}: ${error.message}`)
         })
-    }
-    render() {
-        let toRender = 
-            <div className="centered-top">
-                <LogoutForm />
-                <br />
-                Your rooms:
-                <RoomList rooms={this.state.rooms}/>
-            </div>
-        if (!ClubSession.getPlayerId())
-            toRender = <Redirect to="/"/>
-        return (
-            <React.Fragment>
-                {toRender}
-            </React.Fragment>
-        )
-    }
-
+    }, [])
+    
+    let toRender = 
+        <div className="centered-top">
+            <LogoutForm />
+            <br />
+            Your rooms:
+            <RoomList rooms={rooms}/>
+        </div>
+    if (!ClubSession.getPlayerId())
+        toRender = <Redirect to="/"/>
+    return (
+        <React.Fragment>
+            {toRender}
+        </React.Fragment>
+    )
 }
+
+export default Profile

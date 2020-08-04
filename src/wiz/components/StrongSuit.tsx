2f15ea6cbabca2699a31202543391b7684cf94b7
Ilia Feldgun <iliafeldgun@outlook.com>
2020-08-04 15:15:58 +0300

    Converted to FunctionComponent

--- src/wiz/components/StrongSuit.tsx
+++ src/wiz/components/StrongSuit.tsx
@@ -4,40 +4,40 @@ import { Suit } from '../../interfaces/Card'
 interface IStrongSuitProps {
     strongSuit: Suit
 }
-export default class StrongSuit extends React.PureComponent<IStrongSuitProps> {
-    render() {
-        const suit = this.translateSuit()
-        const isRed = this.isRed()
-        const red = isRed ? "red-card" : ""
+const StrongSuit: React.FC<IStrongSuitProps> = (props) => {
+    const suit = translateSuit(props.strongSuit)
+    const redClass = isRed(props.strongSuit) ? "red-card" : ""
 
-        return (
-            <span className={`strong-suit ${red}`}>
-                {suit}
-                <span className="tooltip-text">Strong suit</span>
-            </span>
-        )
-    }
-    
-    isRed() : boolean {
-        return this.props.strongSuit === Suit.HEART || this.props.strongSuit === Suit.DIAMOND
-    }
-    translateSuit() : string {
-        let suit: string
-        switch(this.props.strongSuit) {
-            case Suit.SPADE:
-                suit = "♠"
-                break
-            case Suit.HEART:
-                suit = "♥"
-                break
-            case Suit.CLUB:
-                suit = "♣"
-                break
-            case Suit.DIAMOND:
-                suit = "♦"
-                break
-        }
-
-        return suit
-    }
+    return (
+        <span className={`strong-suit ${redClass}`}>
+            {suit}
+            <span className="tooltip-text">Strong suit</span>
+        </span>
+    )
 }
+
+const translateSuit = (suit: Suit) : string  => {
+    let textSuit: string
+    switch(suit) {
+        case Suit.SPADE:
+            textSuit = "♠"
+            break
+        case Suit.HEART:
+            textSuit = "♥"
+            break
+        case Suit.CLUB:
+            textSuit = "♣"
+            break
+        case Suit.DIAMOND:
+            textSuit = "♦"
+            break
+    }
+
+    return textSuit
+}
+
+const isRed = (suit: Suit) : boolean => {
+    return suit === Suit.HEART || suit === Suit.DIAMOND
+}
+
+export default StrongSuit

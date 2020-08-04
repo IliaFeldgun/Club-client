3509241c6cc4aed761ca0d9591ef9402d5adf510
Ilia Feldgun <iliafeldgun@outlook.com>
2020-08-04 18:46:09 +0300

    Converted to FunctionComponent

--- src/components/Login/LoginModal.tsx
+++ src/components/Login/LoginModal.tsx
@@ -6,45 +6,28 @@ ReactModal.setAppElement('*')
 interface ILoginModalProps {
     show: boolean
 }
-interface ILoginModalState {
-    showModal: boolean
-}
-export default class LoginModal extends React.PureComponent<ILoginModalProps, ILoginModalState> {
-    constructor(props: ILoginModalProps) {
-        super(props)
+const LoginModal: React.FC<ILoginModalProps> = (props) => {
+    const [showModal, setShowModal] = React.useState(false)
+    React.useEffect(() => {
+        setShowModal(props.show)
+    }, [props.show])
 
-        this.state = {
-            showModal: false
-        }
+    const handleCloseModal = () => {
+        setShowModal(false)
     }
-    componentDidMount() {
-        this.setState({
-            showModal: this.props.show
-        })
-    }
-    handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        this.handleOpenModal()
-    }
-    handleOpenModal = () => {
-        this.setState({ showModal: true })
-    }
-    
-    handleCloseModal = () => {
-        this.setState({ showModal: false })
-    }
-    render() {
-        return (
-            <ReactModal className="box-modal login-modal"
-                        shouldCloseOnEsc={true}
-                        shouldCloseOnOverlayClick={true}
-                        isOpen={this.state.showModal}
-                        onRequestClose={this.handleCloseModal}>
-                <LoginForm/>
-                
-                <button className="form-button close-button" onClick={this.handleCloseModal}>
-                    Close
-                </button>
-            </ReactModal>
-        )
-    }
+    return (
+        <ReactModal className="box-modal login-modal"
+                    shouldCloseOnEsc={true}
+                    shouldCloseOnOverlayClick={true}
+                    isOpen={showModal}
+                    onRequestClose={handleCloseModal}>
+            <LoginForm/>
+            
+            <button className="form-button close-button" onClick={handleCloseModal}>
+                Close
+            </button>
+        </ReactModal>
+    )
 }
+
+export default LoginModal

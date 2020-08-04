e08754f73ff487c653727ad1d6891c46fce3645e
Ilia Feldgun <iliafeldgun@outlook.com>
2020-08-04 14:52:32 +0300

    Converted to functional component

--- src/components/RoomList.tsx
+++ src/components/RoomList.tsx
@@ -3,17 +3,22 @@ import React from "react"
 interface IRoomListProps {
     rooms: string[]
 }
-export default class RoomList extends React.PureComponent<IRoomListProps,{}>{
-    render() {
-        const rooms = this.props.rooms.map((room) => 
+const RoomList: React.FunctionComponent<IRoomListProps> = (props) => {
+    const [rooms, setRooms] = React.useState<JSX.Element[]>([])
+    
+    React.useEffect(() => {
+        setRooms(props.rooms.map((room) => 
             <li key={room}>
                 <a href={`/room/${room}`}>{room}</a>
             </li>    
-        )
-        return (
-            <ol>
-                {rooms}
-            </ol>
-        )
-    }
+        ))
+    }, [props.rooms])
+    
+    return (
+        <ol>
+            {rooms}
+        </ol>
+    )
 }
+
+export default RoomList

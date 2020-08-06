5984becf9c522757e7170f89e92984b8726cc2a2
Ilia Feldgun <iliafeldgun@outlook.com>
2020-08-06 18:18:32 +0300

    Converted to JSS

--- src/components/ClientErrorBox.tsx
+++ src/components/ClientErrorBox.tsx
@@ -2,12 +2,30 @@ import React from 'react'
 import ClientError from '../engine/api/ClientError'
 import ReactModal from 'react-modal'
 
+import {createUseStyles} from 'react-jss'
+import modalClasses from '../style/modalClasses'
+import formClasses from '../style/formClasses'
+
+const useStyles = createUseStyles({
+    boxModal: {...modalClasses.boxModal, 
+        textAlign: 'center',
+        paddingLeft: '5%',
+        paddingRight: '5%',
+        paddingBottom: '5%'
+    },
+    formButton: {...formClasses.formButton, 
+        marginTop: '2%', 
+        float: 'right'
+    }
+})
+
 ReactModal.setAppElement('*')
 interface IClientErrorBoxProps {
     error: ClientError
     onModalClose?: () => void
 }
 const ClientErrorBox: React.FC<IClientErrorBoxProps> = (props) => {
+    const classes = useStyles()
     const [displayError, setDisplayError] = React.useState(true)
 
     const handleCloseModal = () => {
@@ -19,7 +37,7 @@ const ClientErrorBox: React.FC<IClientErrorBoxProps> = (props) => {
     return (
         <React.Fragment>
             <ReactModal 
-                className="box-modal error-modal"
+                className={classes.boxModal}
                 shouldCloseOnEsc={true}
                 shouldCloseOnOverlayClick={true}
                 isOpen={displayError}
@@ -27,7 +45,7 @@ const ClientErrorBox: React.FC<IClientErrorBoxProps> = (props) => {
             >
                 <h1>{props.error.httpStatusCode}</h1>
                 <h3>{props.error.message}</h3>
-                <button className="form-button close-button" onClick={handleCloseModal}>
+                <button className={classes.formButton} onClick={handleCloseModal}>
                     Close
                 </button>
             </ReactModal>

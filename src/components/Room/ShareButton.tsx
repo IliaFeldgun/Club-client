96c1a770353805d55112d80ae98b49b9ae43d95a
Ilia Feldgun <iliafeldgun@outlook.com>
2020-08-06 18:34:20 +0300

    Converted to JSS

--- src/components/Room/ShareButton.tsx
+++ src/components/Room/ShareButton.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactModal from 'react-modal'
 
+import {createUseStyles} from 'react-jss'
+import formClasses from '../../style/formClasses'
+import modalClasses from '../../style/modalClasses'
+const useStyles = createUseStyles({
+    boxModal: {...modalClasses.boxModal,
+        width: '80%'
+    },
+    formButton: formClasses.formButton,
+    urlInput: {
+        width: '99%'
+    },
+    closeButton: {...formClasses.formButton,
+        position: 'relative',
+        left: '50%',
+        transform: 'translate(-50%, 0)'
+    }
+})
+
 interface IShareButtonProps {
     targetUrl: string
 }
 const ShareButton: React.FC<IShareButtonProps> = (props) => {
+    const classes = useStyles()
     const [showModal, setShowModal] = React.useState(false)
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         handleOpenModal()
@@ -20,29 +39,28 @@ const ShareButton: React.FC<IShareButtonProps> = (props) => {
         document.execCommand("copy")
     }
 
-    const buttonClass = "form-button"
     return (
         <React.Fragment>
-            <button className={buttonClass} type="button" onClick={handleClick}>
+            <button className={classes.formButton} onClick={handleClick}>
                 <span>Share room link</span>
             </button>
             
             <ReactModal 
-                className="box-modal share-modal" 
+                className={classes.boxModal}
                 shouldCloseOnEsc={true}
                 shouldCloseOnOverlayClick={true}
                 isOpen={showModal}
                 onRequestClose={handleCloseModal}
             >
                 <h3>Share this link:</h3>
-                <input 
-                    className="url-textbox" 
+                <input
+                    className={classes.urlInput}
                     type="url" 
                     value={props.targetUrl} 
                     onClick={handleInputClick} 
                     readOnly={true}
                 />
-                <button className="form-button" onClick={handleCloseModal}>
+                <button className={classes.closeButton} onClick={handleCloseModal}>
                     Close
                 </button>
             </ReactModal>

aa805ea2738ffbb11e85a088136cd8fab359710a
Ilia Feldgun <iliafeldgun@outlook.com>
2020-08-06 17:53:56 +0300

    Consolidated formButton

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -3,7 +3,9 @@ import LobbyApi from "../../engine/api/LobbyApi"
 import ClubSession from "../../utils/ClubSession"
 import ClientError from "../../engine/api/ClientError"
 import ClientErrorBox from "../ClientErrorBox"
+
 import {createUseStyles} from 'react-jss'
+import formClasses from '../../style/formClasses'
 
 const useStyles = createUseStyles({
     loginForm: {
@@ -17,21 +19,7 @@ const useStyles = createUseStyles({
             outline: 0
         }
     },
-    formButton: {
-        backgroundColor: 'rgb(0, 102, 153)',
-        border: 0,
-        color: 'white',
-        borderRadius: '3px',
-        paddingTop: '3px',
-        paddingBottom: '3px',
-        paddingLeft: '6px',
-        paddingRight: '6px',
-        marginTop: '5px',
-        marginBottom: '5px',
-        '&:hover': {
-            backgroundColor: 'cornflowerblue'
-        }
-    }
+    formButton: formClasses.formButton
 })
 
 const LoginForm: React.FC = () => {

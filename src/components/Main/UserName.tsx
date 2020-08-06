923dfa98c67567cb942cfcefc7c263b1995cbc3e
Ilia Feldgun <iliafeldgun@outlook.com>
2020-08-06 16:42:04 +0300

    Converted to JSS

--- src/components/Main/UserName.tsx
+++ src/components/Main/UserName.tsx
@@ -1,7 +1,18 @@
 import React from "react"
 import ClubSession from "../../utils/ClubSession"
 
+import {createUseStyles} from 'react-jss'
+
+const useStyles = createUseStyles({
+    user: {
+        fontSize: '3vh',
+        float: 'right',
+        lineHeight: '6vh',
+        marginRight: '2vmax',
+    }
+})
 const UserName: React.FunctionComponent = () => {
+    const classes = useStyles()
     const [playerName, setPlayerName] = React.useState("")
     React.useEffect(() => {
         ClubSession.assertSession()
@@ -19,7 +30,7 @@ const UserName: React.FunctionComponent = () => {
         <a href="/login">Login</a>
 
     return (
-        <span className="main-user">
+        <span className={classes.user}>
             {toRender}
         </span>
     )

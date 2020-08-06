46d622716509629368123b35d49cb0f8143e5395
Ilia Feldgun <iliafeldgun@outlook.com>
2020-08-06 17:11:19 +0300

    Converted to JSS

--- src/components/Home/Catalog.tsx
+++ src/components/Home/Catalog.tsx
@@ -1,20 +1,41 @@
 import React from "react"
-import "./Catalog.css"
+import {createUseStyles} from 'react-jss'
+
+const useStyles = createUseStyles({
+    catalog: {
+        float: 'left',
+        padding: '1vmax',
+        marginTop: '2vh',
+        marginBottom: '2vh',
+        marginRight: '2vw',
+        marginLeft: '2vw',
+        borderRadius: '6px',
+        backgroundColor: 'whitesmoke',
+        width: 'fit-content'
+    },
+    catalogItem: {
+        textAlign: 'center'
+    },
+    catalogImg: {
+        width: '15vw'
+    }
+})
 interface ICatalogProps {
     games: {name: string, img: string, url: string}[]
 }
 const Catalog: React.FunctionComponent<ICatalogProps> = (props) => {
+    const classes = useStyles()
     const games = props.games.map((game) => 
-        <div key={game.url} className="catalog-item">
+        <div key={game.url} className={classes.catalogItem}>
             <a href={game.url}>
-                <img src={game.img} alt={game.name}/>
+                <img className={classes.catalogImg} src={game.img} alt={game.name}/>
                 <div>{game.name}</div>
             </a>
         </div>
 
     )
     return (
-        <div className="catalog container">
+        <div className={classes.catalog}>
             {games}
         </div>
     )

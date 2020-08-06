d48e60f2ab92cc43cec9f5ef607870746eb46a78
Ilia Feldgun <iliafeldgun@outlook.com>
2020-08-06 18:10:11 +0300

    Converted to JSS

--- src/components/Login/LoginModal.tsx
+++ src/components/Login/LoginModal.tsx
@@ -2,11 +2,21 @@ import React from 'react'
 import ReactModal from 'react-modal'
 import LoginForm from './LoginForm'
 
+import {createUseStyles} from 'react-jss'
+import modalClasses from '../../style/modalClasses'
+import formClasses from '../../style/formClasses'
+
+const useStyles = createUseStyles({
+    boxModal: {...modalClasses.boxModal, textAlign: 'center'},
+    formButton: {...formClasses.formButton, marginTop: '2%'}
+})
+
 ReactModal.setAppElement('*')
 interface ILoginModalProps {
     show: boolean
 }
 const LoginModal: React.FC<ILoginModalProps> = (props) => {
+    const classes = useStyles()
     const [showModal, setShowModal] = React.useState(false)
     React.useEffect(() => {
         setShowModal(props.show)
@@ -16,14 +26,14 @@ const LoginModal: React.FC<ILoginModalProps> = (props) => {
         setShowModal(false)
     }
     return (
-        <ReactModal className="box-modal login-modal"
+        <ReactModal className={classes.boxModal}
                     shouldCloseOnEsc={true}
                     shouldCloseOnOverlayClick={true}
                     isOpen={showModal}
                     onRequestClose={handleCloseModal}>
             <LoginForm />
             
-            <button className="form-button close-button" onClick={handleCloseModal}>
+            <button className={classes.formButton} onClick={handleCloseModal}>
                 Close
             </button>
         </ReactModal>
